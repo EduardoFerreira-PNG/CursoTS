a6d883b1b7d6641a16e69205850ded2c8a8506ad
Eduardo <eduardonaves02@outlook.com>
2022-08-13 18:11:13 -0300

    atualização do projeto TS

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,31 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    controller.adiciona();
+});
 
 
-const negociacao = new Negociacao(new Date(), 10 , 100);
-console.log(negociacao.volume)
-  
 
-// CRTL C PARA PARAR O SERVER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CRTL C PARA PARAR O SERVER ****************************************
