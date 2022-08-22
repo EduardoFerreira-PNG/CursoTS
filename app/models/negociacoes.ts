ffdec0210cbcb4b43e7146e8ef43d21f88213a84
Eduardo <eduardonaves02@outlook.com>
2022-08-22 18:03:05 -0300

    atualização curso ts PARTE 2

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,11 +3,11 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {  // nao preciso usar a tag ARRAY só colocar as chaves vazias após definir o tipo
     private negociacoes: Negociacao[]  = [];
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): readonly Negociacao[]{ // forma mais facil de escrever um array readonly (leitura)
+    public lista(): readonly Negociacao[]{ // forma mais facil de escrever um array readonly (leitura)
         return this.negociacoes;
     }
 
